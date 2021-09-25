43c2137d04bec5722c885771f76e6c8f75dff70d
klau <laudevelopment@gmail.com>
2021-09-24 23:07:59 -0500

    Write ingested data to database

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
-from house import get_page_source
-from house import get_abs_paths
-from house import get_years_from_url
-from house import extract_zip_url_as_list
+from src.orm.statements import delete_rows_by_year
+from orm.tables import house
+from orm.settings import CONNECTION
+from sqlalchemy import create_engine
+from ingest.house import get_page_source
+from ingest.house import get_abs_paths
+from ingest.house import get_years_from_url
+from ingest.house import extract_zip_url_as_list
+from orm.statements import insert_transactions
+from orm.statements import get_data_by_year
 
 base_url = "https://disclosures-clerk.house.gov/PublicDisclosure/FinancialDisclosure"
 
@@ -9,9 +15,18 @@ page_source = get_page_source(base_url)
 abs_paths = get_abs_paths(page_source, base_url)
 years = get_years_from_url(abs_paths)
 
-database = {}
+data = {}
 for (year, abs_path) in zip(years, abs_paths):
-    database[year] = extract_zip_url_as_list(abs_path)
+    data[year] = extract_zip_url_as_list(abs_path)
 
-print([(key, len(database[key])) for key in database.keys()], sep='\n')
+engine = create_engine(CONNECTION, future=True)
+for year in years:
+    delete_rows_by_year(engine, house, year)
+
+for (year, transactions) in data.items():
+    insert_transactions(engine, house, transactions)
+
+print([(key, len(data[key])) for key in data.keys()], sep='\n')
+print([(key, len(get_data_by_year(engine, house, key)))
+      for key in data.keys()])
 # zippath = 'C:\\Users\\lauke\\Downloads\\2016FD.zip'
